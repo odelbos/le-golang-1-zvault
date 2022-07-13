47a1ef3a43c4927c7a0f30ffc82f79d4bfc148c8
odelbos <od@phibox.com>
2022-07-13 12:40:00 +0200

    feat: add GenPBKDF2() and EncryptWothKey() functions
    - GenPBKDF2 is used to generate a derived key from a password
    - EncryptWithKey is used to encrypt some []byte with AES_256_GCM using
      a specific key.

--- crypto.go
+++ crypto.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
+	"crypto/sha256"
+	"golang.org/x/crypto/pbkdf2"
 )
 
 const (
+	PBKDF2_SALT_SIZE = 16
 	PBKDF2_ITER = 65536
 	AES_KEYSIZE = 32
 	AES_IV_SIZE = 16
@@ -19,4 +24,29 @@ func GenCryptoRand(nb uint8) []byte {
 		panic("Cannot generate random bytes !")
 	}
 	return data
+}
+
+// Generate a derived key from a pawwsord.
+// Return the derived key and the salt.
+func GenPBKDF2(pwd []byte) ([]byte, []byte) {
+	salt := GenCryptoRand(PBKDF2_SALT_SIZE)
+	key := pbkdf2.Key(pwd, salt, PBKDF2_ITER, AES_KEYSIZE, sha256.New)
+	return key, salt
+}
+
+// Encrypt the 'plain' message with AES_256_GCM and the given 'key'.
+// This function will generate a crypto random IV used for encryption.
+// Return the encrypted message and the IV.
+func EncryptWithKey(plain *[]byte, key *[]byte) (*[]byte, *[]byte, error) {
+	iv := GenCryptoRand(AES_IV_SIZE)
+	block, err := aes.NewCipher(*key)
+	if err != nil {
+		return &[]byte{}, &[]byte{}, err
+	}
+	gcm, err := cipher.NewGCMWithNonceSize(block, AES_NONCE_SIZE)
+	if err != nil {
+		return &[]byte{}, &[]byte{}, err
+	}
+	cipher := gcm.Seal(nil, iv, *plain, []byte(AES_GCM_AAD))
+	return &cipher, &iv, nil
 }
