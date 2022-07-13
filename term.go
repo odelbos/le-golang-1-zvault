32f971ea0970539d14ab01407e2569a6157f8e31
odelbos <od@phibox.com>
2022-07-13 02:18:47 +0200

    feat: add prompt password functions

--- term.go
+++ term.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"bufio"
 	"strings"
+	"bytes"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 // Ask question to user and return inout value
@@ -17,4 +19,36 @@ func Ask(question string) (string, error) {
 	}
 	value := strings.TrimSpace(input)
 	return value, nil
+}
+
+// Ask user for a password twice and verify equality
+func AskPwdTwice() ([]byte, error) {
+	fmt.Print("> Enter Password: ")
+	pwd1, err := terminal.ReadPassword(0)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Print("\n> Repeat Password: ")
+	pwd2, err := terminal.ReadPassword(0)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("")
+
+	if ! bytes.Equal(pwd1, pwd2) {
+		return nil, fmt.Errorf("Passwords do not match !")
+	}
+	return pwd1, nil
+}
+
+// Prompt for a password
+func AskPwd() ([]byte, error) {
+	fmt.Print("> Enter Password: ")
+	pwd, err := terminal.ReadPassword(0)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("")
+	return pwd, nil
 }
